ef2da1f7cb70e7640d5dfe8a83046e1ac09afc2e
TheArchons <progamer9138@gmail.com>
2023-12-13 09:44:10 -0800

    move to env

--- vex_tournament/settings.py
+++ vex_tournament/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+env = json.load(open(BASE_DIR / '.env'))
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-08#^@aq&n(1rzdp0(9hyxg1+&3lh#54j(7_4zhv76s#pjr30=s'
+SECRET_KEY = env['key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env['debug']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env['allowed_hosts']
 
 
 # Application definition

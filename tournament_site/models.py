fbb59ce87dce024571105ab4e919fc848de94e8b
TheArchons <progamer9138@gmail.com>
2023-12-13 17:53:12 -0800

    add models

--- tournament_site/models.py
+++ tournament_site/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class School(models.Model):
+    school_name = models.CharField(max_length=100)
+    number_of_teams = models.IntegerField()
+    teams = models.ManyToManyField("Team")
+
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=100)
+    team_number = models.IntegerField()
+    ranking_points = models.IntegerField(default=0)
+    matches_played = models.IntegerField(default=0)
